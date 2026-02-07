1e7603a94e22189ee24fc29e1f32571fe5187737
shahidansari311 <shahidansari945256@gmail.com>
2026-02-07 16:19:27 +0530

    Home-Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Marketplace from './pages/Marketplace'
+import About from './pages/About'
 import Mylisting from './pages/Mylisting'
 import Myorders from './pages/Myorders'
 import Managelisting from './pages/Managelisting'
 import Listingdetails from './pages/Listingdetails'
 import Messages from './pages/Messages'
 import Loading from './pages/Loading'
+import Navbar from './components/Navbar'
 
 const App = () => {
+
+  const {pathname}=useLocation();
+
   return (
     <div>
+      {!pathname.includes('/admin') && <Navbar/>}
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/Marketplace' element={<Marketplace/>}/>
@@ -23,6 +29,7 @@ const App = () => {
         <Route path='/messages' element={<Messages/>}/>
         <Route path='/Myorders' element={<Myorders/>}/>
         <Route path='/loading' element={<Loading/>}/>
+        <Route path='/about-us' element={<About/>}/>
 
       </Routes>
     </div>
