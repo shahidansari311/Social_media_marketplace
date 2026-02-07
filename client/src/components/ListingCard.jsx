a867f32a3974aefd055df6c48dd6c2658621effe
shahidansari311 <shahidansari945256@gmail.com>
2026-02-08 03:45:11 +0530

    Adding clerk

--- client/src/components/ListingCard.jsx
+++ client/src/components/ListingCard.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
+import { platformIcons } from '../assets/assets'
+import { BadgeCheck, LineChart, MapPin, User } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
-const ListingCard = () => {
-  return (
-    <div></div>
-  )
+const ListingCard = ({ listing }) => {
+
+    const navigate=useNavigate();
+    const currency = import.meta.env.VITE_CURRENCY || '$'
+
+    return (
+        <div className='relative bg-white rounded-2xl shadow-sm border border-gray-100 overflow-hidden hover:shadow-md transition'>
+            {/* Featured Banner */}
+            {listing.featured && (
+                <>
+                    <p className='py-1' />
+                    <div className='absolute top-0 left-0 w-full bg-gradient-to-r from-pink-500 to-purple-500 text-white text-center text-xs font-semibold py-1 tracking-wide uppercase'>
+                        Featured
+                    </div>
+                </>
+            )}
+
+            <div className='p-5 pt-8'>
+                {/* Header*/}
+                <div className='flex items-center gap-3 mb-3'>
+                   { platformIcons[listing.platform]}
+
+                   <div className="flex flex-col gap-0.5">
+                        <h2>{listing.title}</h2>
+                        <p>@{listing.username} -
+                            <span className='capitalize'> {listing.platform}</span> </p>
+                    </div> 
+                    {listing.verified && <BadgeCheck className='text-green-500 ml-auto w-5 h-5'/>}
+                </div>
+                {/* Stats */}
+                <div className='flex felx-wrap justify-between max-w-lg items-center gap-3 my-5'>
+                    <div className='flex items-center text-sm text-gray-600'>
+                        <User className='size-6 mr-1 text-gray-400'/>
+                        <span className='text-lg font-medium text-slate-800 mr-1.5'>{listing.followers_count.toLocaleString()}</span> followers
+                    </div>
+                    {
+                        listing.engagement_rate && (
+                            <div className='flex items-centertext-sm text-gray-600'>
+                                <LineChart className='size-6 mr-1 text-gray-400'/>
+                                <span className='text-lg font-medium text-dlate-800 mr-1.5'>
+                                    {listing.engagement_rate}
+                                </span> % engagement
+                            </div>
+                        )
+                    }
+                </div>
+                {/* tags and location*/}
+                <div className='flex items-center gap-3 mb-3'>
+                    <span className='text-xs font-medium bg-pink-100 text-pink-600 px-3 py-1 rounded-full capitalize'>{listing.niche}</span>
+                    {listing.country && (
+                        <div className='flexitems-center text-gray-500 text-sm '>
+                            <MapPin className='size-6 mr-1 text-gray-400'/>
+                            {listing.country}
+                        </div>
+                    )}
+                </div>
+                {/* Description */}
+                <p className='text-sm text-gray-600 mb-4 line-clamp-2'>
+                    {listing.description}
+                </p>
+                <hr className='my-5 border-gray-200'/>
+
+                {/*Footer*/}
+                <div className='flex items-center justify-between'>
+                    <div className='flex items-baseline'>
+                        <span className='text-2xl font-medium text-slate-800'>
+                            {currency}
+                            {listing.price.toLocaleString()}
+                        </span>
+                    </div>
+                    <button
+                    onClick={()=>{navigate('/listing/${listing.id');window.scrollTo(0,0)}} className='px-2 py-3 bg-indigo-600 text-white text-sm rounded-lg hover:bg-indigo-700 transition'>
+                        More Details
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default ListingCard
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Latestlisting from '../components/Latestlisting'
+import Plans from '../components/Plans'
+import CTA from '../components/CTA'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
       <Latestlisting/>
+      <Plans/>
+      <CTA/>
+      <Footer/>
     </div>
   )
 }
