0965479b5622030c20dd814d67daa31b7df7fc57
shahidansari311 <shahidansari945256@gmail.com>
2026-02-14 14:43:03 +0530

    orders oage

--- client/src/pages/Myorders.jsx
+++ client/src/pages/Myorders.jsx
@@ -1,8 +1,160 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {dummyOrders, platformIcons} from '../assets/assets'
+import { CheckCircle2, ChevronDown, ChevronUp, Copy, DollarSign, Loader2Icon } from 'lucide-react';
+import { format } from 'date-fns';
+import toast from 'react-hot-toast';
 
 const Myorders = () => {
+
+  const currency=import.meta.env.VITE_CURRENCY || '$';
+  const [orders,setOrders] = useState([]);
+  const [loading,setloading] =useState(true);
+  const [expandedId,setexpandedId] =useState(null);
+
+  const fetchOders=async()=>{
+    setOrders(dummyOrders);
+    setloading(false);
+  }
+
+  useEffect(()=>{
+    fetchOders();
+  },[])
+
+  const mask =(val,type)=>{
+    if(!val && val !==0 ) return "-";
+    return type.toLowerCase() === "password"  ? "●".repeat(8) : String(val);
+  }
+
+  const copy = async (txt)=>{
+    try {
+      await navigator.clipboard.writeText(txt);
+      toast.success("Copied to clipboard");
+    } catch (error) {
+      toast.error("Copied failed");
+    }
+  }
+
+  if(loading){
+    return (
+      <div className='h-[80vh] flex items-center justify-center'>
+        <Loader2Icon className='size-7 animate-spin text-indigo-600'/>
+      </div>
+    )
+  }
+
+  if(!orders.length){
+    return (
+      <div className='px-4 md:px-16 lg:px-24 xl:px-32'>
+        <div className='max-w-2xl mx-auto mt-14 bg-white rounded-xl border border-gray-200 p-8 text-center'>
+          <h3 className='text-lg font-semibold'>No orders yet</h3>
+          <p className='text-sm text-gray-500 mt-2'>
+            You haven't purchased any listings yet.
+          </p>
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>Myorders</div>
+    <div className='px-4 md:px-16 lg:px-24 xl:px-32 py-6'>
+      <h2 className='text-2xl font-semibold mb-6'>My orders</h2>
+
+      <div className='space-y-4'>
+        {orders.map((orders)=>{
+          const id=orders.id;
+          const listing =orders.listing;
+          const credential = orders.credential;
+          const iseExpanded= expandedId === id;
+
+
+          return (
+            <div key={id} className=' bg-white rounded-lg border border-gray-200 p-5 flex flex-col max-w-4xl'>
+              <div className='flex items-center gap-4 flex-1'>
+                <div className='p-2 rounded-lg bg-gray-50 max-sm:hidden'>
+                  {platformIcons[listing.platform]}
+                </div>
+                <div className='flex-1'>
+                  <div className='flex items-start justify-between gap-4'>
+                    <div>
+                      <h3 className='text-lg font-semibold'>{listing.title}</h3>
+                      <p className='text-sm text-gray-500 mt-1'>
+                        @{listing.username} ● <span className='capitalize'>   {listing.platform}</span>
+                      </p>
+                      <div className='flex gap-2 mt-2'>
+                        {listing.verified && (
+                          <span className='flex items-center text-xs bg-indigo-50 text-indigo-600 px-2 py-1 rounded-md'>
+                            <CheckCircle2 className='w-3 h-3 mr-1'/> Verfied
+                          </span>
+                        )}
+                        {listing.monetized && (
+                          <span className='flex items-center text-xs bg-green-50 text-green-600 px-2 py-1 rounded-md'>
+                            <DollarSign className='w-3 h-3 mr-1'/> Monetized
+                          </span>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className='text-right'>
+                          <p className='text-2xl font-bold'>
+                            {currency} 
+                            {Number(orders.amount).toLocaleString() }
+                          </p>
+                          <p className='text-sm text-gray-500'>USD</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='flex flex-col gap-2 items-end'>
+                <button onClick={()=>setexpandedId((p)=>( p === id ? null : id))} className='flex items-center gap-2 bg-white border border-gray-200 px-3 py-2 rounded hover:shadow text-sm' aria-expanded={iseExpanded}>
+                  {iseExpanded ? (
+                    <>
+                    <ChevronUp className='size-4 '/> Hide credentials
+                    </>
+                  ) : (
+                    <>
+                    <ChevronDown className='size-4'/> View credentials
+                    </>
+                  )}
+                </button>
+                <div className='text-xs text-gray-500 mt-2 text-right'>
+                  <div>
+                    Credential Purchased : {format(new Date(orders.createdAt), "MMM d, yyyy")}
+                  </div>
+                </div>
+              </div>
+              {iseExpanded && (
+                <div className='mt-4 md:mt-0 pt-4'>
+                  <div className='space-y-2'>
+                    {credential.updatedCredential.map((cred)=>(
+                      <div key={cred.name} className='flex items-center justify-between gap-3 bg-gray-50 rounded-md p-2'>
+                        <div>
+                          <p className='text-sm font-medium text-gray-800'>
+                            {cred.name}
+                          </p>
+                          <p className='text-xs text-gray-500'>{cred.type}</p>
+                        </div>
+                        <div className='flex items-center gap-2'>
+                          <code className='text-sm font-mono'>
+                            {mask(cred.value , cred.type)}
+                          </code>
+                          <button className='px-2 py-1 text-xs bg-white border border-gray-200 rounded hover:shadow' title="Copy Credential" onClick={(e)=>{
+                            e.stopPropagation();
+                            copy(cred.value);
+                          }}>
+                            <Copy className='size-4'/>
+                          </button>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
